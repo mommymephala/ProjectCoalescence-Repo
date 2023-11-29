05e31868bd150fa802bbc2627c68e59a9dd02fad
Yağız <ygzkrt@gmail.com>
2023-11-29 23:38:55 +0300

    Canvas Scripti

--- Assets/Inventory/Inventory_Scripts/InventoryTest.cs
+++ Assets/Inventory/Inventory_Scripts/InventoryTest.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class InventoryTest : MonoBehaviour
 {
     private RectTransform inventoryRect;
+    
 
     private float inventoryWidht, inventoryHeight;
 
@@ -34,6 +36,7 @@ public class InventoryTest : MonoBehaviour
 
     private void CreateLayout()
     {
+        
         allslots = new List<GameObject>();
 
         inventoryWidht = (slots / rows) * (slotSize + slotPaddingLeft) + slotPaddingLeft;
@@ -44,6 +47,37 @@ public class InventoryTest : MonoBehaviour
         
         inventoryRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, inventoryWidht);
         inventoryRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, inventoryHeight);
+
+
+        int columns = slots / rows;
+
+
+
+        for (int y = 0; y < rows; y++)
+        {
+            float yPosition = (-2) - slotPaddingTop * (y + 1) - (slotSize * y);
+
+            for (int x = 0; x < columns; x++)
+            {
+                GameObject newSlot = (GameObject)Instantiate(SlotPrefab);
+
+                RectTransform slotRect = newSlot.GetComponent<RectTransform>();
+
+                newSlot.name = "Slot";
+
+                newSlot.transform.SetParent(this.transform.parent);
+
+                slotRect.localPosition = inventoryRect.localPosition +
+                                         new Vector3(slotPaddingLeft * (x + 1) + (slotSize * x),
+                                             -slotPaddingTop * (y + 1) - (slotSize * y));
+                
+                slotRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal,slotSize);
+                slotRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical,slotSize);
+                
+                allslots.Add(newSlot);
+            }
+
+        }
 
     }
 }
