49bf4ae869e87d796116c504830be2ecc64f3d1f
Okan Ataç <okanatac.cl@gmail.com>
2023-12-04 16:09:46 +0300

    particle packages import, before converting to URP materials

---
+++ Assets/PlayerActions/PlayerLook.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+namespace PlayerActions
+{
+    public class PlayerLook : MonoBehaviour
+    {
+        [Header("References")] [SerializeField]
+        private Transform camHolder;
+
+        [SerializeField] private Transform orientation;
+
+        [Header("Mouse Sensitivity")] [SerializeField]
+        private float mouseSens = 100f;
+
+        [SerializeField] private float adsSens = 50f;
+
+        //Mouse look variables
+        private const float Multiplier = 0.01f;
+        private float _mouseX;
+        private float _mouseY;
+        private float _xRotation;
+        private float _yRotation;
+        private bool _aimingDownSight;
+
+        private void Update()
+        {
+            HandleMouseLook();
+        }
+
+        private void HandleMouseLook()
+        {
+            // Get mouse input
+            _mouseX = Input.GetAxisRaw("Mouse X");
+            _mouseY = Input.GetAxisRaw("Mouse Y");
+
+            // Determine current mouse sensitivity
+            var currentMouseSensitivity = _aimingDownSight ? adsSens : mouseSens;
+
+            // Apply mouse input to rotation variables
+            _yRotation += _mouseX * currentMouseSensitivity * Multiplier;
+            _xRotation -= _mouseY * currentMouseSensitivity * Multiplier;
+
+            // Clamp the X rotation to avoid over-rotation
+            _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
+
+            // Update camera and orientation rotations
+            camHolder.transform.rotation = Quaternion.Euler(_xRotation, _yRotation, 0);
+            orientation.transform.rotation = Quaternion.Euler(0, _yRotation, 0);
+        }
+        public void SetAimingDownSight(bool aiming)
+        {
+            _aimingDownSight = aiming;
+        }
+    }
+}
