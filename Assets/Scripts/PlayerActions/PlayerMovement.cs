6c30d3c326bba81d52d33795f2ef57cc9896ed8c
Okan Ataç <okanatac.cl@gmail.com>
2023-12-11 17:17:06 +0300

    ground check fixed, stairs and slopes aren't

--- Assets/Scripts/PlayerActions/PlayerMovement.cs
+++ Assets/Scripts/PlayerActions/PlayerMovement.cs
@@ -29,6 +29,9 @@ namespace PlayerActions
         [SerializeField] public float moveSpeed = 1f;
         [SerializeField] private float airMultiplier = 1f;
         [SerializeField] private Vector3 gravity;
+        [SerializeField] private float maxSlopeAngle;
+        [SerializeField] private float maxStepHeight;
+        [SerializeField] private float stepCheckDistance;
 
         [Header("Sprinting")]
         [SerializeField] private float walkSpeed = 4f;
@@ -173,24 +176,62 @@ namespace PlayerActions
 
         private bool OnSlope()
         {
-            if (!Physics.Raycast(transform.position, Vector3.down, out _slopeHit, PlayerHeight / 2 + 0.5f))
-                return false;
-            return _slopeHit.normal != Vector3.up;
+            if (Physics.Raycast(transform.position, Vector3.down, out _slopeHit, PlayerHeight / 2 + 0.5f))
+            {
+                float slopeAngle = Vector3.Angle(Vector3.up, _slopeHit.normal);
+
+                // Considering a surface as a slope if it has an angle and it's less than the max slope angle
+                return _slopeHit.normal != Vector3.up && slopeAngle <= maxSlopeAngle;
+            }
+            return false;
+        }
+        
+        private bool CheckForStep(out Vector3 stepUpOffset)
+        {
+            stepUpOffset = Vector3.zero;
+            RaycastHit hit;
+
+            // New step detection logic
+            Vector3 rayStart = transform.position + (transform.forward * 0.1f) + (Vector3.up * maxStepHeight);
+            Vector3 rayDirection = Vector3.down;
+
+            if (Physics.Raycast(rayStart, rayDirection, out hit, maxStepHeight))
+            {
+                if (hit.point.y > transform.position.y && hit.point.y < transform.position.y + maxStepHeight)
+                {
+                    stepUpOffset = new Vector3(0f, maxStepHeight - (rayStart.y - hit.point.y), 0f);
+                    return true;
+                }
+            }
+            return false;
         }
 
         private void MovePlayer()
         {
-            switch (_isGrounded)
+            if (_isGrounded)
             {
-                case true when !OnSlope():
+                // Check if there's a slope
+                if (OnSlope())
+                {
+                    // Apply force in the direction of the slope
+                    _rb.AddForce(Vector3.ProjectOnPlane(gravity, _slopeHit.normal) * (moveSpeed * MovementMultiplier), ForceMode.Acceleration);
+                }
+                else
+                {
+                    // Normal grounded movement
                     _rb.AddForce(_moveDirection.normalized * (moveSpeed * MovementMultiplier), ForceMode.Acceleration);
-                    break;
-                case true when OnSlope():
-                    _rb.AddForce(_slopeMoveDirection.normalized * (moveSpeed * MovementMultiplier), ForceMode.Acceleration);
-                    break;
-                case false:
-                    _rb.AddForce(_moveDirection.normalized * (moveSpeed * MovementMultiplier * airMultiplier), ForceMode.Acceleration);
-                    break;
+                }
+
+                // Enhanced Step Logic
+                if (CheckForStep(out Vector3 stepUpOffset))
+                {
+                    _rb.position += stepUpOffset; // Step up
+                }
+            }
+            else
+            {
+                // Air movement logic
+                _rb.AddForce(_moveDirection.normalized * (moveSpeed * MovementMultiplier * airMultiplier), ForceMode.Acceleration);
             }
         }
 
@@ -204,18 +245,70 @@ namespace PlayerActions
         
         private void CheckGround()
         {
-            var origin = new Vector3(transform.position.x, transform.position.y - (transform.localScale.y * .5f), transform.position.z);
-            Vector3 direction = transform.TransformDirection(Vector3.down);
-            const float distance = .75f;
+            float colliderRadius = 0.5f; // Adjust as necessary
+            float colliderHeight = PlayerHeight;
+            Vector3 boxCenter = transform.position + (Vector3.down * (colliderHeight / 2 - colliderRadius / 2));
+            Vector3 halfExtents = new Vector3(colliderRadius, 0.1f, colliderRadius);
+            Quaternion orientation = Quaternion.identity;
+            float maxDistance = 0.2f; // Adjust for better ground detection on slopes
 
-            if (Physics.Raycast(origin, direction, out RaycastHit _, distance))
+            if (Physics.BoxCast(boxCenter, halfExtents, Vector3.down, out RaycastHit hit, orientation, maxDistance))
             {
-                _isGrounded = true;
+                if (Vector3.Angle(Vector3.up, hit.normal) <= maxSlopeAngle)
+                {
+                    _isGrounded = true;
+                }
+                
+                else
+                {
+                    _isGrounded = false;
+                }
+                if (Vector3.Angle(Vector3.up, _slopeHit.normal) <= maxSlopeAngle)
+                {
+                    _isGrounded = true;
+                }
+                else
+                {
+                    _isGrounded = false;
+                }
             }
             else
             {
                 _isGrounded = false;
             }
+
+            // Debug visualization
+            DebugDrawBox(boxCenter, halfExtents, orientation, maxDistance, _isGrounded ? Color.green : Color.red);
+        }
+
+        private void DebugDrawBox(Vector3 center, Vector3 halfExtents, Quaternion orientation, float maxDistance, Color color)
+        {
+            Vector3 up = orientation * Vector3.up;
+            Vector3 right = orientation * Vector3.right;
+            Vector3 forward = orientation * Vector3.forward;
+
+            Vector3 p1 = center + up * halfExtents.y - right * halfExtents.x - forward * halfExtents.z;
+            Vector3 p2 = center + up * halfExtents.y + right * halfExtents.x - forward * halfExtents.z;
+            Vector3 p3 = center + up * halfExtents.y - right * halfExtents.x + forward * halfExtents.z;
+            Vector3 p4 = center + up * halfExtents.y + right * halfExtents.x + forward * halfExtents.z;
+            Vector3 p5 = center - up * halfExtents.y - right * halfExtents.x - forward * halfExtents.z;
+            Vector3 p6 = center - up * halfExtents.y + right * halfExtents.x - forward * halfExtents.z;
+            Vector3 p7 = center - up * halfExtents.y - right * halfExtents.x + forward * halfExtents.z;
+            Vector3 p8 = center - up * halfExtents.y + right * halfExtents.x + forward * halfExtents.z;
+
+            // Draw the lines considering the maxDistance
+            Debug.DrawLine(p1, p2, color, maxDistance);
+            Debug.DrawLine(p2, p4, color, maxDistance);
+            Debug.DrawLine(p4, p3, color, maxDistance);
+            Debug.DrawLine(p3, p1, color, maxDistance);
+            Debug.DrawLine(p5, p6, color, maxDistance);
+            Debug.DrawLine(p6, p8, color, maxDistance);
+            Debug.DrawLine(p8, p7, color, maxDistance);
+            Debug.DrawLine(p7, p5, color, maxDistance);
+            Debug.DrawLine(p1, p5, color, maxDistance);
+            Debug.DrawLine(p2, p6, color, maxDistance);
+            Debug.DrawLine(p4, p8, color, maxDistance);
+            Debug.DrawLine(p3, p7, color, maxDistance);
         }
         
         public void InventoryCanvasCheck()
